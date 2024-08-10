c591b545b38992c4361a50cb63f720f92f7ff743
Loopeq <arsen.00z11@gmail.com>
2024-08-10 15:57:23 +0300

    fix: change secret bot_token settings

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,10 @@
+import os
 from pydantic_settings import BaseSettings
-from pathlib import Path
 
-CURRENT_PATH = Path(__file__).resolve()
-ROOT = CURRENT_PATH.parent.parent
-ENV_ROOT = ROOT / ".env"
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
-
-    class Config:
-        env_file = ENV_ROOT
+    BOT_TOKEN: str = os.environ['BOT_TOKEN']
 
 
 settings = Settings(_case_sensitive=False)
