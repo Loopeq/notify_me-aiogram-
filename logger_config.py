e81d54d3159c05035bede3e41537fd4d87633ff5
Loopeq <arsen.00z11@gmail.com>
2024-08-10 21:28:40 +0300

    feature: add pin message and some

--- logger_config.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-import sys
-
-logger.remove()
-logger.add(sys.stdout, level="INFO", format="{time} {level} {message}")
-logger.add("debug.log", format="{time} {level} {message}",
-           level="INFO", rotation="100 MB")
